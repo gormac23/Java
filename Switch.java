37708c31a7b02d25cd9c35340493b4a27676c951
Colin Gorman <33034259+gormac23@users.noreply.github.com>
2019-04-19 22:06:26 +0100

    Update Switch.java

--- Switch.java
+++ Switch.java
@@ -34,7 +34,12 @@ public class Main {
                 break;
         }
 
-        // More code here
+        // Create a new switch statement using char instead of int
+        // create a new char variable
+        // create a switch statement testing for
+        // A, B, C, D, or E
+        // display a message if any of these are found and then break
+        // Add a default which displays a message saying not found
 
         char charVar = 'C';
 
