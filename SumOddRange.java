622e92c21c09b56d62397d438ed10a8509424ff2
Colin Gorman <33034259+gormac23@users.noreply.github.com>
2019-05-01 12:29:29 +0100

    Add files via upload

---
+++ SumOddRange.java
@@ -0,0 +1,28 @@
+public class SumOddRange {
+
+    public static boolean isOdd(int number) {
+        if(number < 0) {
+            return false;
+        } else if (number % 2 != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static int sumOdd(int start, int end) {
+        int total = 0;
+        if((start < 0 || end < 0) || (end < start)) {
+            return -1;
+        } else {
+            for(int i = start; i <= end; i ++) {
+                if(isOdd(i)) {
+                    total += i;
+
+                }
+            }
+            return total;
+        }
+        //return -1 ;
+    }
+}
