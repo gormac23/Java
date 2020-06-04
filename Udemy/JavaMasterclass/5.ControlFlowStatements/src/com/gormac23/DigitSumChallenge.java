14ec59ac9fd6c7cd91ce781856bb244f30bb0f45
gormac23 <colin.gorman23@mail.dcu.ie>
2020-06-04 13:40:48 +0100

    Continued Section 5.ControlFlowStatements

--- Udemy/JavaMasterclass/5.ControlFlowStatements/src/com/gormac23/DigitSumChallenge.java
+++ Udemy/JavaMasterclass/5.ControlFlowStatements/src/com/gormac23/DigitSumChallenge.java
@@ -1,4 +1,36 @@
 package com.gormac23;
 
 public class DigitSumChallenge {
+
+    // Create a method to sum the digits of a number
+    // i.e. 125 returns 1 + 2 + 5 = 8
+
+    public static void main(String[] args) {
+
+        System.out.println(sumDigits(125));
+        System.out.println(sumDigits(2048));
+        System.out.println(sumDigits(10));
+        System.out.println(sumDigits(5));
+        System.out.println(sumDigits(-1));
+
+    }
+
+
+    private static int sumDigits(int n) {
+        // Doesn't accept negative numbers
+        // or single digit numbers
+        if(n < 10) {
+            return -1;
+        }
+
+        int sum = 0;
+
+        while(n >= 1) {
+            // extract least-significant digit and drop it
+            sum += n % 10;
+            n /= 10;
+        }
+
+        return sum;
+    }
 }
--- Udemy/JavaMasterclass/5.ControlFlowStatements/src/com/gormac23/WhileLoop.java
+++ Udemy/JavaMasterclass/5.ControlFlowStatements/src/com/gormac23/WhileLoop.java
@@ -1,4 +1,61 @@
 package com.gormac23;
 
 public class WhileLoop {
+
+    public static void main(String[] args) {
+        int count = 0;
+        while(count != 6) {
+            System.out.println("Count value is " + count);
+            count++;
+        }
+
+        // Two different ways to use the while loop
+        // These but have the same output
+        count = 0;
+        while(true) {
+            if(count == 6) {
+                break;
+            }
+            System.out.println("Count value is " + count);
+            count++;
+        }
+
+        // There is also a do while loop
+        // This always gets executed at least once even if the first expression
+        // equals to false
+        count = 0;
+        do {
+            System.out.println("Count value is " + count); // This line always prints
+            count++;
+        } while(count != 6);
+
+
+        // WHILE LOOP CHALLENGE
+        System.out.println("\nWHILE LOOP CHALLENGE\n");
+
+        count = 0;
+        int number = 4;
+        int finish =  20;
+
+        while(number <= finish) {
+            number++;
+            if(!isEvenNumber(number)) {
+                continue;
+            }
+
+            System.out.println("Even number " + number);
+            count++;
+            if(count == 5) {
+                System.out.println("5 Evens found... exiting loop");
+                break;
+            }
+
+        }
+    }
+
+
+
+    private static boolean isEvenNumber(int n) {
+        return (n % 2 == 0);
+    }
 }
